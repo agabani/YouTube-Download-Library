afbde20eeaaa198b2b3af0a10d2657f740f8be83
agabani <agabani@live.com>
2014-02-22 17:02:31 +0000

    Added search API and functionality

--- Test Application/TestApplication.cpp
+++ Test Application/TestApplication.cpp
@@ -9,19 +9,44 @@ int main (int argc, char * argv[])
 {
 	std::vector<YouTubeDownloadLibrary::Video> videos = YouTubeDownloadLibrary::YouTubeDownload::GetLink("E4XqGyCa5Vo");
 
-	std::cout << "Found " << videos.size() << " videos." << std::endl;
+	YouTubeDownloadLibrary::Video video;
 
-	for (unsigned int i = 0; i < videos.size(); i++)
+	int result =
+		YouTubeDownloadLibrary::YouTubeDownload::SearchVideo (
+		videos,
+		YouTubeDownloadLibrary::YouTubeDownload::Container::MP4,
+		true,
+		video
+		);
+
+	if (result == 0)
 	{
-		std::cout << "--- Video number: " << i+1 << " ---" << std::endl;
-		std::cout << videos.at(i).GetITag() << std::endl;
-		std::cout << videos.at(i).GetQuality() << std::endl;
-		std::cout << videos.at(i).GetType() << std::endl;
-		std::cout << videos.at(i).GetURL() << std::endl;
-		std::cout << std::endl;
+		std::cout << "Video Found" << std::endl;
+		std::cout << "Container: ";
+		switch (video.GetContainer())
+		{
+		case YouTubeDownloadLibrary::YouTubeDownload::Container::FLV:
+			std::cout << "FLV" << std::endl;
+			break;
+		case YouTubeDownloadLibrary::YouTubeDownload::Container::MP4:
+			std::cout << "MP4" << std::endl;
+			break;
+		case YouTubeDownloadLibrary::YouTubeDownload::Container::_3PG:
+			std::cout << "3PG" << std::endl;
+			break;
+		case YouTubeDownloadLibrary::YouTubeDownload::Container::WEBM:
+			std::cout << "WEBM" << std::endl;
+			break;
+		default:
+			break;
+		}
+		std::cout << "itag: " << video.GetITag() << std::endl;
+		std::cout << "Resolution: " << video.GetResolution() << std::endl;
+		std::cout << "Type: " << video.GetType() << std::endl;
+		std::cout << "URL:" << std::endl << video.GetURL() << std::endl;
+	} else {
+		std::cout << "Video not Found" << std::endl;
 	}
-
-	std::cin.get();
 
 	return 0;
 }
